a9157bbaebb36c76b458ad04d0b2ba479ab08972
Kehinde Odewole <kennyedward99@gmail.com>
2019-09-14 18:55:42 +0100

    feat(landing-page): implement landing page
        -  create landing page
    
    [Starts #168416406]

---
+++ src/components/auth/Signup/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Signup extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is the sign up page</h1>
+      </div>
+    )
+  }
+}
